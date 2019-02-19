96772008a6a0a2de8d3482cecd0fee5f7c5eec42
jose <josebaduma@gmail.com>
2019-02-19 15:48:53 -0600

    First commit. Workspace catkin

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,17 @@
+# CMake generated Testfile for 
+# Source directory: /home/lutec5/catkin_ws/src
+# Build directory: /home/lutec5/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_ros_demos/rrbot_control")
+subdirs("gazebo_ros_demos/rrbot_description")
+subdirs("gazebo_ros_demos/rrbot_gazebo")
+subdirs("gazebo_ros_demos/custom_plugin_tutorial")
+subdirs("myrobot_description")
+subdirs("myrobot_gazebo")
+subdirs("r2d2")
+subdirs("robot_setup_tf")
+subdirs("learning_tf")
+subdirs("testbot_description")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/lutec5/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/lutec5/catkin_ws/devel;/home/lutec5/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/lutec5/catkin_ws/devel;/home/lutec5/ws_moveit/devel;/opt/ros/kinetic" != "" else []
