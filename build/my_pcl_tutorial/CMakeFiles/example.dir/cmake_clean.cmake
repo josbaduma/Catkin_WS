4226855002d392030043bbc717a2f44634a6fcf9
jose <josebaduma@gmail.com>
2019-02-20 14:05:57 -0600

    Update

---
+++ build/my_pcl_tutorial/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/src/example.cpp.o"
+  "/home/lutec5/catkin_ws/devel/lib/my_pcl_tutorial/example.pdb"
+  "/home/lutec5/catkin_ws/devel/lib/my_pcl_tutorial/example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/rosbot_description/src/rosbot_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lutec5/catkin_ws/src/rosbot_description/src/rosbot_navigation
+# Build directory: /home/lutec5/catkin_ws/build/rosbot_description/src/rosbot_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
