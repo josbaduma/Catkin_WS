87e0044fd239a1eb7043788a26e3b35ad775279c
jose <josebaduma@gmail.com>
2019-02-20 11:40:20 -0600

    Update

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,10 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("gazebo_ros_demos/rrbot_control")
-subdirs("gazebo_ros_demos/rrbot_description")
-subdirs("gazebo_ros_demos/rrbot_gazebo")
-subdirs("gazebo_ros_demos/custom_plugin_tutorial")
 subdirs("myrobot_description")
 subdirs("myrobot_gazebo")
 subdirs("r2d2")
--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/lutec5/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lutec5/catkin_ws/devel;/home/lutec5/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/lutec5/catkin_ws/devel;/home/lutec5/ws_moveit/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/lutec5/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/lutec5/ws_moveit/devel;/opt/ros/kinetic" != "" else []
